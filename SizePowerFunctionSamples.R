d197fa3a1683aff915babda07cda763dff853cf0
davidhhhhhhh <57957067+davidhhhhhhh@users.noreply.github.com>
2025-02-18 11:25:46 -0500

    two sample wilcoxon power and size, updated power size function template

--- SizePowerFunctionSamples.R
+++ SizePowerFunctionSamples.R
@@ -1,32 +1,5 @@
-# 1/23/25
-a = read.csv("Moon.csv")
-dim(a)
-names(a)
-diff = a$Moon - a$Other
-# q1a
-t.test(diff, mu=0, alternative = "greater")
-
-# check normality assumption 
-shapiro.test(diff)
-qqnorm(diff)
-qqline(diff)
-
-# mu = 0 for signed test
-x = sum(diff > 0)
-# pbinom(15,15,0.5) means probability of success smaller or equal to 15
-p = 1- pbinom(13, 15, 0.5)
-
-# use BSDA package
-library(BSDA)
-SIGN.test(diff, alternative = "greater")
-
-# sort diff
-diff_sort = sort(diff)
-
-# to get confidence interval do two.sided 
-SIGN.test(diff)
-
-# simulation, N is number of repeats
+# One Sample Size Function ----
+# n is number Sample, N repeats, dist for distrbution in String 
 TsignSize = function (n, N, dist){
   sizeT = 0; sizeSign = 0
   library(BSDA); mExp = qexp(0.5, rate=1)
@@ -58,22 +31,7 @@ TsignSize = function (n, N, dist){
   return(result)
 }
 
-# sample size 20
-set.seed(1)
-TsignSize(20, 10000, "normal")
-set.seed(2)
-TsignSize(20, 10000, "t")
-set.seed(3)
-TsignSize(20, 10000, "exp")
-
-# sample size 40
-set.seed(4)
-TsignSize(40, 10000, "normal")
-set.seed(5)
-TsignSize(40, 10000, "t")
-set.seed(6)
-TsignSize(40, 10000, "exp")
-
+# One Sample Power Function-----
 # power plot for normal distribution 
 TSignPowerNorm = function(n, N, alt){
   m = length(alt)
@@ -98,62 +56,7 @@ TSignPowerNorm = function(n, N, alt){
   lines(alt, powerSign, col="red", lty=2)
   legend("topright", legend=c("t test", "sign test"), lty=c(1,2), col=c("black", "red"), lwd=c(2,2))
 }
-
-set.seed(1)
-TSignPowerNorm(40, 10000, seq(-1.2,0,0.02))
-
-# power plot for t distribution with df3
-TSignPowerT = function(n, N, alt){
-  m = length(alt)
-  powerT = rep(0, m); powerSign = rep(0,m)
-  library(BSDA)
-  for (j in 1:m){
-    for (i in 1:N){
-      y = rt(n,3) + alt[j]
-      pt = t.test(y, alternative="less")$p.value
-      psign = SIGN.test(y, alternative = "less")$p.value
-      if (pt <= 0.05){
-        powerT[j] = powerT[j] + 1
-      }
-      if (psign <= 0.05){
-        powerSign[j] = powerSign[j] + 1
-      }
-    }
-  }
-  powerT = powerT / N; powerSign = powerSign / N
-  result = list(Ttest = powerT, SignTest = powerSign)
-  plot(alt, powerT, type="l", xlab="Alternative", ylab="power", main="Alternative v.s. Power for t-3 distribution")
-  lines(alt, powerSign, col="red", lty=2)
-  legend("topright", legend=c("t test", "sign test"), lty=c(1,2), col=c("black", "red"), lwd=c(2,2))
-}
-
-set.seed(1)
-TSignPowerT(20, 10000, seq(-1.2,0,0.02))
-
-# wilcox 
-dsignrank(0:6,n=3)
-
-# poiner estimate
-wilcox.test(diff, alternative = "greater", conf.int = TRUE)
-
-# find walsh average
-library(Rfit)
-walsh(diff)
-sort(walsh(diff))[26]
-sort(walsh(diff))[95]
-
-# wilcoxon test example
-experiment = c(15, 18, 8, 15, 17, 16, 13)
-control = c(10, 5, 4, 9, 12, 6, 7)
-t.test(experiment, control, var.equal = TRUE, alternative = "greater")
-wilcox.test(experiment, control, alternative = "greater")
-
-# conf int
-t.test(experiment, control, var.equal = TRUE, conf.level = 0.95)
-wilcox.test(experiment, control, conf.int = TRUE, conf.level = 0.95)
-
-# Compare pooled T test and Wilcoxon test size and power -----
-# simulation, N is number of repeats
+# Two Sample Size Function-----
 pooledTWilcoxonSize = function (n, m, N, dist){
   sizeT = 0; sizeWilcoxon = 0
   library(BSDA); mExp = qexp(0.5, rate=1)
@@ -188,27 +91,7 @@ pooledTWilcoxonSize = function (n, m, N, dist){
   return(result)
 }
 
-# equal sample sizes 
-set.seed(1);
-pooledTWilcoxonSize(20, 20, 10000, "normal");
-set.seed(1);
-pooledTWilcoxonSize(20, 20, 10000, "t");
-set.seed(1);
-pooledTWilcoxonSize(20, 20, 10000, "exp");
-
-# equal sample sizes 
-set.seed(2);
-pooledTWilcoxonSize(20, 30, 10000, "normal");
-set.seed(2);
-pooledTWilcoxonSize(20, 30, 10000, "t");
-set.seed(2);
-pooledTWilcoxonSize(20, 30, 10000, "exp");
-
-# conclusion: two samples both wilcox and t are robust 
-# and have good size
-
-# power 
-# power plot for normal distribution 
+# Two Sample Power Function -----
 TSWilcoxPowerNorm = function(num_x, num_y, N, alt, distr){
   m = length(alt)
   powerT = rep(0, m); powerWilcox = rep(0,m)
@@ -243,12 +126,3 @@ TSWilcoxPowerNorm = function(num_x, num_y, N, alt, distr){
   lines(alt, powerWilcox, col="red", lty=2)
   legend("top", legend=c("t test", "wilcox test"), lty=c(1,2), col=c("black", "red"), lwd=c(2,2))
 }
-
-alt = seq(-1.2, 1.2, 0.1)
-TSWilcoxPowerNorm(20, 20, 10000, alt, "Normal")
-TSWilcoxPowerNorm(20, 20, 10000, alt, "t3")
-TSWilcoxPowerNorm(20, 20, 10000, alt, "exp")
-
-# Conclusion if distribution is normal, t test has slightly more power than Wilcoxon
-# if distribution is not normal, Wilcoxon test has higher power 
-# for wilcoxon, higher variance of original distribution means less power 
