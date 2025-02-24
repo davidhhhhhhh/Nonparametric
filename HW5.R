8c398461149d894df7b7cae11dfb9e625fe9e243
davidhhhhhhh <57957067+davidhhhhhhh@users.noreply.github.com>
2025-02-24 12:57:46 -0500

    adding HW for two sample location problem

---
+++ HW5.R
@@ -0,0 +1,44 @@
+# HW 5
+# Q4
+pooledTWilcoxonSize = function (num_x, num_y, N, alt_dist){
+  sizeT = 0; sizeWilcoxon = 0
+  library(BSDA)
+  for (i in 1:N){
+    if (alt_dist == "normal"){
+      x = rnorm(num_x)
+      y = rnorm(num_y, sd=3)
+      pt = t.test(x, y, var.equal = TRUE, alternative = "greater")$p.value
+      pWil = wilcox.test(x, y, alternative = "greater")$p.value
+    }
+    else if (alt_dist == "t"){
+      x = rnorm(num_x)
+      y = rt(num_y, df=3)
+      pt = t.test(x, y, var.equal = TRUE, alternative = "greater")$p.value
+      pWil = wilcox.test(x, y, alternative = "greater")$p.value
+    }
+    else {
+      x = rnorm(num_x)
+      y = runif(num_y, min=-1, max=1)
+      pt = t.test(x, y, var.equal = TRUE, alternative = "greater")$p.value
+      pWil = wilcox.test(x, y, alternative = "greater")$p.value
+    }
+    if (pt <= 0.05){
+      sizeT = sizeT + 1
+    }
+    if (pWil <= 0.05){
+      sizeWilcoxon = sizeWilcoxon + 1
+    }
+  }
+  sizeT = sizeT / N; sizeWilcoxon = sizeWilcoxon / N
+  result = list(Ttest = sizeT, WilcoxTest = sizeWilcoxon)
+  return(result)
+}
+
+num_x = 30
+num_y = 25
+set.seed(1)
+pooledTWilcoxonSize(num_x, num_y, 10000, alt_dist = "normal")
+set.seed(1)
+pooledTWilcoxonSize(num_x, num_y, 10000, alt_dist = "t")
+set.seed(1)
+pooledTWilcoxonSize(num_x, num_y, 10000, alt_dist = "uniform")
