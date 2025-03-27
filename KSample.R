d6fdaf0f982b6b882497bdbb82666fdf1f8a4bc9
davidhhhhhhh <57957067+davidhhhhhhh@users.noreply.github.com>
2025-03-27 10:58:49 -0400

    anova k sample

---
+++ KSample.R
@@ -0,0 +1,15 @@
+# K sample 
+# Create vectors for each detergent
+A <- c(77, 81, 71, 76, 80)
+B <- c(72, 58, 74, 66, 70)
+C <- c(76, 85, 82, 80, 77)
+
+# Combine into a data frame
+detergent_data <- data.frame(
+  Detergent = rep(c("A", "B", "C"), each = 5),
+  Value = c(A, B, C)
+)
+
+# ANOVA for K Sample
+out = aov(Value~Detergent, data = detergent_data)
+anova(out)
