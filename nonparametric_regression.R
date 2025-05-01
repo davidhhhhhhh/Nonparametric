bb7925f122b7c62d8db8e9cca3cbebf011ff7029
davidhhhhhhh <57957067+davidhhhhhhh@users.noreply.github.com>
2025-05-01 11:00:46 -0400

    non para regression

--- nonparametric_regression.R
+++ nonparametric_regression.R
@@ -43,15 +43,40 @@ a = ethanol[order(ethanol$E),]
 out2 = loess(a$NOx~a$E,span=0.75)
 out3 = loess(a$NOx~a$E,span=0.1)
 out4 = loess.as(x=a$E, y=a$NOx, criterion = "gcv")
-lines(a$E, predict(out2, a$E), col="black", lty=1, lwd=2)
-lines(a$E, predict(out3, a$E), col="red", lty=2, lwd=2)
-lines(a$E, predict(out4, a$E), col="blue", lty=3, lwd=2)
+ss = seq(min(ethanol$E), max(ethanol$E), 0.01)
+# lines(sort(a$E), predict(out2, sort(a$E)), col="black", lty=1, lwd=2)
+lines(sort(a$E), predict(out3, sort(a$E)), col="red", lty=2, lwd=2)
+lines(sort(a$E), predict(out4, sort(a$E)), col="blue", lty=3, lwd=2)
+lines(ss, predict(out2, ss), col="black", lty=1, lwd=2)
 legend("topright", legend = c("span=0.75", "span=0.1","span=gcv"), 
        col = c("black","red","blue"), lwd=2, lty=c(1,2,3))
 
+# Kernel Smoothing
+library(np)
+plot(ethanol$E, ethanol$NOx, main="Local Regression with Different Spans", 
+     xlab="Equivalence ratio", ylab = "nitrogen oxides")
+a = ethanol[order(ethanol$E),]
+out5 = npreg(bws=0.01, ckertype="gaussian", txdat=a$E, tydat=a$NOx)
+out6 = npreg(bws=0.03, ckertype="gaussian", txdat=a$E, tydat=a$NOx)
+out7 = npreg(bws=0.05, ckertype="gaussian", txdat=a$E, tydat=a$NOx)
+lines(a$E, fitted(out5, a$E), col="black", lty=1, lwd=2)
+lines(a$E, fitted(out6, a$E), col="red", lty=2, lwd=2)
+lines(sort(a$E), fitted(out7, sort(a$E)), col="blue", lty=3, lwd=2)
+legend("topright", legend = c("span=0.01", "span=0.03","span=0.05"), 
+       col = c("black","red","blue"), lwd=2, lty=c(1,2,3))
 
-
-
-
+# Data Driven bw 
+library(np)
+plot(ethanol$E, ethanol$NOx, main="Local Regression with Different Spans", 
+     xlab="Equivalence ratio", ylab = "nitrogen oxides")
+a = ethanol[order(ethanol$E),]
+out5 = npreg(ckertype="gaussian", txdat=a$E, tydat=a$NOx, bwmethod="cv.aic")
+out6 = npreg(ckertype="gaussian", txdat=a$E, tydat=a$NOx, bwmethod="cv.ls")
+out7 = npreg(bws=0.05, ckertype="gaussian", txdat=a$E, tydat=a$NOx)
+lines(a$E, fitted(out5, a$E), col="black", lty=1, lwd=2)
+lines(a$E, fitted(out6, a$E), col="red", lty=2, lwd=2)
+lines(sort(a$E), fitted(out7, sort(a$E)), col="blue", lty=3, lwd=2)
+legend("topright", legend = c("span=aic", "span=ls","span=0.05"), 
+       col = c("black","red","blue"), lwd=2, lty=c(1,2,3))
 
 
